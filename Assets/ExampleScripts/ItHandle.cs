72b2095ba1d9f633019151eed3c5ff8b6c5b9c4d
Georg <georg.auge01@gmail.com>
2022-07-01 13:00:02 +0200

    Initial commit
    - so matvey can see how it's done

---
+++ Assets/ExampleScripts/ItHandle.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using DualPantoFramework;
+using System.Threading.Tasks;
+
+public class ItHandle : MonoBehaviour
+{
+    PantoHandle lowerHandle;
+    bool free = true;
+    public float speed = 0.1f;
+    Rigidbody ballRb;
+
+    void Start()
+    {
+        lowerHandle = GameObject.Find("Panto").GetComponent<LowerHandle>();
+        ballRb = GetComponent<Rigidbody>();
+    }
+
+    public async Task ActivateBall() {
+        // gets called in levelscript
+        
+        await lowerHandle.SwitchTo(gameObject);
+        
+        float sx = Random.Range(0, 2) == 0 ? -1 : 1;
+        float sy = 1; 
+        ballRb.velocity = new Vector3(sx * speed, 0, sy * speed);
+        // Debug.Log("Velocity: " + ballRb.velocity + "Sxy: " + sx + " " + sy);
+    }
+
+    void FixedUpdate()
+    {
+        // transform.position = lowerHandle.HandlePosition(transform.position);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (free)
+            {
+                lowerHandle.Freeze();
+            }
+            else
+            {
+                lowerHandle.Free();
+            }
+            free = !free;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            float sx = Random.Range(0, 2) == 0 ? -1 : 1;
+            float sy = 1; 
+            ballRb.velocity = new Vector3(sx * speed, 0, sy * speed);
+            Debug.Log("Velocity: " + ballRb.velocity);
+        }
+    }
+}
---
+++ Assets/ExampleScripts/MeHandle.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using DualPantoFramework;
+using System.Threading.Tasks;
+
+public class MeHandle : MonoBehaviour
+{
+    bool free = true;
+    PantoHandle upperHandle;
+
+    async void Start()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        transform.position = new Vector3(0, 0.2f, -13);
+        Debug.Log("Pos1: " + transform.position);
+        upperHandle = GameObject.Find("Panto").GetComponent<UpperHandle>();
+        await upperHandle.MoveToPosition(transform.position);
+        GetComponent<BoxCollider>().enabled = true;
+        Debug.Log("Pos2: " + transform.position);
+    }
+
+    async Task ActivatePaddle() {
+        await upperHandle.SwitchTo(gameObject);
+        // upperHandle.FreeRotation();
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = (upperHandle.HandlePosition(transform.position));
+        transform.eulerAngles = new Vector3(0, upperHandle.GetRotation(), 0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (free)
+            {
+                upperHandle.Freeze();
+            }
+            else
+            {
+                upperHandle.Free();
+            }
+            free = !free;
+        }
+    }
+}
